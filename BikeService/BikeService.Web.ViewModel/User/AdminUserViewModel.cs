5171b40e27e299988ddb2ff2e8ee17fbfd8692b0
VeselinAlexiev <vesselin.alexiev06@gmail.com>
2025-04-29 20:37:01 +0300

    Add AdminUserViewModel

---
+++ BikeService/BikeService.Web.ViewModel/User/AdminUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeService.Web.ViewModel.User
+{
+    public class AdminUserViewModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+
+        public string SelectedRole { get; set; }
+        public string SelectedWorkshopName { get; set; }
+
+        public List<string> AllRoles { get; set; } = new List<string>();
+        public List<string> AllWorkshops { get; set; } = new List<string>();
+    }
+}
