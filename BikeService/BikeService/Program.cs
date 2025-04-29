126804d388b75a539ab9d201af6acefa26405d5d
VeselinAlexiev <vesselin.alexiev06@gmail.com>
2025-04-29 20:22:25 +0300

    Configure Program and appsettings files

--- BikeService/BikeService/Program.cs
+++ BikeService/BikeService/Program.cs
@@ -1,7 +1,12 @@
-using BikeService.Data.Entities;
-using BikeService.Data;
+﻿using BikeService.Data.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using BikeService.Common;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using BikeService.Services.Implementation;
+using NETCore.MailKit.Extensions;
+using NETCore.MailKit.Infrastructure.Internal;
+using BikeService.Services.Infrastructure;
 
 public class Program
 {
@@ -9,20 +14,59 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(connectionString));
+
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
         builder.Services.AddIdentity<User, IdentityRole>()
             .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultTokenProviders();
+
+        builder.Services.AddHttpClient();
+        builder.Services.AddScoped<IShopService, ShopService>();
+        builder.Services.AddScoped<ICartService, CartService>();
+        builder.Services.AddScoped<IOrderService, OrderService>();
+
+        builder.Services.AddMailKit(optionBuilder =>
+        {
+            optionBuilder.UseMailKit(new MailKitOptions()
+            {
+                Server = builder.Configuration["MailKit:Server"],
+                Port = Convert.ToInt32(builder.Configuration["MailKit:Port"]),
+                SenderName = builder.Configuration["MailKit:SenderName"],
+                SenderEmail = builder.Configuration["MailKit:SenderEmail"],
+                Account = builder.Configuration["MailKit:Account"],
+                Password = builder.Configuration["MailKit:Password"],
+                Security = true
+            });
+        });
+
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.AccessDeniedPath = "/AccessDenied";
+            options.LoginPath = "/Identity/Account/Login";
+        });
+
+        builder.Services.AddAuthentication()
+            .AddGoogle(options =>
+            {
+                options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+                options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+            })
+            .AddFacebook(options =>
+            {
+                options.AppId = builder.Configuration["Authentication:Facebook:AppId"];
+                options.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"];
+            });
+
         builder.Services.AddRazorPages();
 
         var app = builder.Build();
 
-        // Seed data here
         using (var scope = app.Services.CreateScope())
         {
             var services = scope.ServiceProvider;
@@ -32,7 +76,6 @@ public class Program
             SeedData.Initialize(services, userManager, roleManager, context);
         }
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseMigrationsEndPoint();
@@ -45,9 +88,7 @@ public class Program
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
-
         app.UseRouting();
-
         app.UseAuthentication();
         app.UseAuthorization();
 
