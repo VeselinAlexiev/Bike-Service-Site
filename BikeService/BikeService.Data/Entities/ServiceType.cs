a0f120d1de669c988fdabdf36d9f8feae25e1810
VeselinAlexiev <vesselin.alexiev06@gmail.com>
2025-04-29 20:31:45 +0300

    ServiceType entity

---
+++ BikeService/BikeService.Data/Entities/ServiceType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeService.Data.Entities
+{
+    public class ServiceType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        public ICollection<WorkshopService> WorkshopServices { get; set; } = new List<WorkshopService>();
+    }
+}
