d2af914e6b415a26f2f8c6ff37b3ff384a39dcac
VeselinAlexiev <vesselin.alexiev06@gmail.com>
2025-04-29 20:36:50 +0300

    Add Service related ViewModels

---
+++ BikeService/BikeService.Web.ViewModel/Service/ServiceDetailsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeService.Web.ViewModel.Service
+{
+    public class ServiceDetailsViewModel
+    {
+        public int WorkshopId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public List<ServiceItem> Services { get; set; } = new();
+        public List<BicycleModelInfo> BicycleModels { get; set; } = new();
+
+        public class ServiceItem
+        {
+            public string Title { get; set; } = string.Empty;
+            public string Description { get; set; } = string.Empty;
+            public decimal Price { get; set; }
+            public TimeSpan TimeRequired { get; set; }
+        }
+
+        public class BicycleModelInfo
+        {
+            public int Id { get; set; }
+            public string Model { get; set; } = string.Empty;
+            public string Brand { get; set; } = string.Empty;
+            public int Year { get; set; }
+            public string Type { get; set; } = string.Empty;
+            public string ImageUrl { get; set; } = string.Empty;
+        }
+    }
+}
---
+++ BikeService/BikeService.Web.ViewModel/Service/ServiceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeService.Web.ViewModel.Service
+{
+    public class ServiceViewModel
+    {
+        public int Id { get; set; }
+        public string WorkshopName { get; set; } = string.Empty;
+        public string ServiceType { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public TimeSpan TimeRequired { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
