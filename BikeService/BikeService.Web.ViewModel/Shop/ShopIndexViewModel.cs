dfc8b162885174d9c09da1d9d1633fa615f46bae
VeselinAlexiev <vesselin.alexiev06@gmail.com>
2025-04-29 20:37:14 +0300

    Add Shop related ViewModels

---
+++ BikeService/BikeService.Web.ViewModel/Shop/ShopIndexViewModel.cs
@@ -0,0 +1,35 @@
+﻿using BikeService.Web.ViewModel.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeService.Web.ViewModel.Shop
+{
+    public class ShopIndexViewModel
+    {
+        public IEnumerable<ShopItemViewModel> Items { get; set; } = new List<ShopItemViewModel>();
+
+        public string? Category { get; set; }
+        public string? SortOption { get; set; }
+        public string? SearchTerm { get; set; }
+
+        public int TotalResults { get; set; }
+
+        public IEnumerable<string> Categories { get; set; } = new List<string>()
+        {
+            "Bicycle",
+            "Spare Part"
+        };
+
+        public IEnumerable<string> SortOptions { get; set; } = new List<string>()
+        {
+            "Price Low-High",
+            "Price High-Low",
+            "Name A-Z",
+            "Name Z-A",
+            "Newest"
+        };
+    }
+}
