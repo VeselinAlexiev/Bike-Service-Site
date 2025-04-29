657813118bf6fcfad487ea9831534ad53328b1f7
VeselinAlexiev <vesselin.alexiev06@gmail.com>
2025-04-29 20:30:17 +0300

    OrderBicycle entity

---
+++ BikeService/BikeService.Data/Entities/OrderBicycle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeService.Data.Entities
+{
+    public class OrderBicycle
+    {
+        [Key, Column(Order = 0)]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int BicycleId { get; set; }
+        public Bicycle Bicycle { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
