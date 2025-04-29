daef55146b2cf8cd6eb67c3309cf24f3b1fad613
VeselinAlexiev <vesselin.alexiev06@gmail.com>
2025-04-29 20:32:21 +0300

    Add Workshop entity

---
+++ BikeService/BikeService.Data/Entities/Workshop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeService.Data.Entities
+{
+    public class Workshop
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(250)]
+        public string Location { get; set; } = string.Empty;
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public ICollection<WorkshopService> WorkshopServices { get; set; } = new List<WorkshopService>();
+    }
+}
